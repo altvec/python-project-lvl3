5e7199340defede519f156cc55f818382bc4af2d
altvec <me@sergeykalistratov.com>
2019-09-26 22:35:23 +0500

    simplified downloader.py

--- page_loader/downloader.py
+++ page_loader/downloader.py
@@ -8,15 +8,6 @@ from urllib.parse import urlparse
 import requests
 
 
-def save_page(file_path, content):
-    """Save data into path."""
-    output_dir, file_name = file_path
-    path = os.path.join(output_dir, file_name)
-    with open(path, 'w') as f:
-        f.write(content)
-    return path
-
-
 def generate_file_name(url):
     """Generate filename."""
     parsed_url = urlparse(url)
@@ -30,4 +21,7 @@ def download(output_path, webpage):
     """Download page and save into path."""
     file_name = generate_file_name(webpage)
     content = requests.get(webpage).text
-    return save_page((output_path, file_name), content)
+    file_path = os.path.join(output_path, file_name)
+    with open(file_path, 'w') as f:
+        f.write(content)
+    return file_path
