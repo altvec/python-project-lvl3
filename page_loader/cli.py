19fe05d4ad68a2c6c2e8eb507c5172fac86956fb
altvec <me@sergeykalistratov.com>
2020-01-25 00:41:46 +0500

    Added short options with help messages

--- page_loader/cli.py
+++ page_loader/cli.py
@@ -9,12 +9,19 @@ from page_loader.downloader import download
 from page_loader.logging import configure_logger
 
 parser = argparse.ArgumentParser(description='Page loader')
-parser.add_argument('--output', action='store', help='set output dir')
 parser.add_argument('url', type=str)
 parser.add_argument(
+    '-o',
+    '--output',
+    type=str,
+    help='set output directory',
+)
+parser.add_argument(
+    '-l',
     '--log-level',
     choices=logging.LEVELS,
     default=logging.INFO,
+    help='set log level',
 )
 
 
